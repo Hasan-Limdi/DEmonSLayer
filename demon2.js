68cbad37366e922c0e93c63fcc3e28d9ceb0b3b3
Hasan-Limdi <Hasanlimdi321@gmail.com>
2022-07-16 11:48:44 +0530

    initial commit

---
+++ demon2.js
@@ -0,0 +1,8 @@
+document.querySelector('#image').onclick = function() {
+    document.querySelector('.overlay').classList.toggle('show')
+  }
+  
+  document.querySelector('.overlay').onclick = function() {
+    document.querySelector('.overlay').classList.toggle('show')
+  }
+
